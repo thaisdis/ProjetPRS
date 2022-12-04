aa4ff4128a812725256e6bab9252dfd420594a35
thaisdis <94852146+thaisdis@users.noreply.github.com>
2022-12-04 15:20:39 +0100

    probmlème pour join la thread send; on arrive pas a envoyer le FIN jsp pk avant c'était bon , revoir les fins de boucle

---
+++ network.h
@@ -0,0 +1,16 @@
+#ifndef sockParam_h_
+#define sockParm_h_
+//Cette structure permet de passer les arguments d'une socket UDP d'une fonction à une autre
+typedef struct sockParam SockParam;
+struct sockParam {
+   int sock; //contient le descripteur de fichier de la socket
+   struct sockaddr_in addr;//contient l'adresse du client qui va envoyer/recevoir des données
+   char *filename; 
+};
+
+int ths (struct sockParam sock, char *buffer, int port);
+struct sockParam openDataSocket(int port);
+void *sendFile (void *arg);
+void *receiveACK (void *arg);
+
+#endif
